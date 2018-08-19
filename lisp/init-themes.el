f293dad65bb4aaa2d6f37bc2c55217cff8de1c28
Zhimeng Shi <zshi@salesforce.com>
2018-08-18 17:16:40 -0700

    1. modify the solarized to support xterm-256color with iTerm2 correctly. 2. add my init-local.el and convert to unix format. 3. make Ttem2 send c-pgup and c-pgdown by sending [5;5~ [6;5~ 4. disable dimmer mode and add tlight & tdark for the terminal color mode.

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -29,30 +29,29 @@
 (defun light ()
   "Activate a light color theme."
   (interactive)
-  (setq custom-enabled-themes '(sanityinc-tomorrow-day))
+  (setq custom-enabled-themes '(sanityinc-solarized-light))
   (reapply-themes))
 
 (defun dark ()
   "Activate a dark color theme."
   (interactive)
-  (setq custom-enabled-themes '(sanityinc-tomorrow-bright))
+  (setq custom-enabled-themes '(sanityinc-solarized-dark))
   (reapply-themes))
 
+;; To make solarized works best in xterm-256color mode we need change the iTerm2 color palette by run this command:
+;; printf '\x1B]4;234;rgb:00/2b/36;235;rgb:07/36/42;240;rgb:58/6e/75;241;rgb:65/7b/83;244;rgb:83/94/96;245;rgb:93/a1/a1;254;rgb:ee/e8/d5;230;rgb:fd/f6/e3;136;rgb:b5/89/00;166;rgb:cb/4b/16;160;rgb:dc/32/2f;125;rgb:d3/36/82;61;rgb:6c/71/c4;33;rgb:26/8b/d2;37;rgb:2a/a1/98;64;rgb:85/99/00\a'
+;; just put it into .bash_rc or .zshrc file.
+(defun tlight ()
+  "Activate a term light color theme."
+  (interactive)
+  (setq custom-enabled-themes '(sanityinc-solarized-tlight))
+  (reapply-themes))
 
-(when (maybe-require-package 'dimmer)
-  (setq-default dimmer-fraction 0.15)
-  (add-hook 'after-init-hook 'dimmer-mode)
-  (with-eval-after-load 'dimmer
-    ;; TODO: file upstream as a PR
-    (advice-add 'frame-set-background-mode :after (lambda (&rest args) (dimmer-process-all))))
-  (with-eval-after-load 'dimmer
-    ;; Don't dim in terminal windows. Even with 256 colours it can
-    ;; lead to poor contrast.  Better would be to vary dimmer-fraction
-    ;; according to frame type.
-    (defun sanityinc/display-non-graphic-p ()
-      (not (display-graphic-p)))
-    (add-to-list 'dimmer-exclusion-predicates 'sanityinc/display-non-graphic-p)))
-
+(defun tdark ()
+  "Activate a term dark color theme."
+  (interactive)
+  (setq custom-enabled-themes '(sanityinc-solarized-tdark))
+  (reapply-themes))
 
 (provide 'init-themes)
 ;;; init-themes.el ends here
